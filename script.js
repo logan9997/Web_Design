1c204b08093cf9df1a3b6201db6202d075c34790
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-01-17 18:56:15 +0000

    personal web page

---
+++ script.js
@@ -0,0 +1,75 @@
+//add a colour to each span tag in panel2 
+// window.onload = function() {
+//     const colours = ["red", "pink", "blue", "purple", "yellow"]
+//     const spans = document.querySelectorAll("span");
+//     for (let i = 0; i < spans.length; i++) {
+//         var random_num = Math.floor(Math.random() * colours.length - 1) + 1
+//         console.log(random_num)
+//         spans[i].style.color = colours[random_num];
+//     }
+// }
+
+
+function login_values() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    if (email != "a" || password != "a") {
+        console.log("incorrect")
+        document.getElementById("login-error-msg").innerText = "incorrect email and password";
+    } else {
+        console.log("correct")
+        var login_content = document.getElementById("login-content-container");
+        login_content.style.display = "flex";
+        login_content.scrollIntoView()
+    }
+}
+
+window.onload = function() {
+    const layers = 6;
+    const layer_images = 8;
+    const images = [
+        "int.jpg", "def.jpg", "return.jpg", "true.jpg",
+        "else.jpg", "zip.jpg", "async.jpg", "while.jpg",
+        "bool.jpg"
+    ];
+
+    for (let x = 0 ; x < layers; x ++) {
+
+        for (let i = 0; i < layer_images ; i ++) {
+            //create img tag
+            var img = document.createElement("img")
+
+            //add to parent div
+            document.getElementById("programming").appendChild(img)
+
+            //randomly select image
+            var img_index = Math.floor(Math.random() * (images.length - 1)) ;
+            
+            //set image
+            img.src = images[img_index];   
+
+            //set random postion
+            var top = (i+1) * 4; 
+            var left = (i * 2)
+
+            //if half way through the arch, change direction
+            if (i > (layer_images / 2)-1) {
+                //reverse direction
+                left = (2 * (layer_images - i) - 2) 
+            }
+
+            //flip arch to other side of the page
+            if (x+1 < (layers / 2)+1) {
+                left *= -1
+            }
+
+            left += (x+1)*1
+
+            //apply positioning to image
+            img.style.top = `${top}rem`;
+            img.style.left = `${left}rem`;
+            
+        }
+    }
+}
